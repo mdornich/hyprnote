4404dcb5aab628b14c6ef8af326dd3e45eea3362
Yujong Lee <yujonglee.dev@gmail.com>
2026-02-23 23:14:00 +0900

    feature gating silero-rs

--- crates/vad-ext/src/lib.rs
+++ crates/vad-ext/src/lib.rs
@@ -1,14 +1,16 @@
+#[cfg(feature = "chunking")]
 mod continuous;
 mod error;
 mod masking;
 mod streaming;
 
+#[cfg(feature = "chunking")]
 pub use continuous::*;
 pub use error::*;
 pub use masking::*;
 pub use streaming::*;
 
-#[cfg(test)]
+#[cfg(all(test, feature = "chunking"))]
 pub mod tests {
 
     use futures_util::StreamExt;
